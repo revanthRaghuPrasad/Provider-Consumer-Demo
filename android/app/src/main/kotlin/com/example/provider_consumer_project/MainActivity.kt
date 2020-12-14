ff72bd6f7759da276672d67d51fc704f7d79407f
Revanth R Prasad <revanthrprasad@gmail.com>
2020-12-14 20:20:23 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/provider_consumer_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.provider_consumer_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String data = 'Top Secret Data';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(data),
+        ),
+        body: Level1(data),
+      ),
+    );
+  }
+}
+
+class Level1 extends StatelessWidget {
+  final String data;
+  Level1(this.data);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Level2(data),
+    );
+  }
+}
+
+class Level2 extends StatelessWidget {
+  final String data;
+  Level2(this.data);
+  @override
+  Widget build(BuildContext context) {
+    return Level3(data);
+  }
+}
+
+class Level3 extends StatelessWidget {
+  final String data;
+  Level3(this.data);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(data),
+    );
+  }
+}

d5c9395c72c00515c9b07a53c2f5cb68095db7dc
Revanth R Prasad <revanthrprasad@gmail.com>
2020-12-15 11:34:54 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String data = 'Top Secret Data';
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(data),
+    return ChangeNotifierProvider<Data>(
+      create: (context) => Data(),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: MyText(),
+          ),
+          body: Level1(),
         ),
-        body: Level1(data),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
-  final String data;
-  Level1(this.data);
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(data),
+      child: Level2(),
     );
   }
 }
 
 class Level2 extends StatelessWidget {
-  final String data;
-  Level2(this.data);
   @override
   Widget build(BuildContext context) {
-    return Level3(data);
+    return Column(
+      children: [
+        MyTextField(),
+        Level3(),
+      ],
+    );
   }
 }
 
 class Level3 extends StatelessWidget {
-  final String data;
-  Level3(this.data);
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(data),
+      child: Text(Provider.of<Data>(context).data),
     );
+  }
+}
+
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<Data>(context, listen: true).data);
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (newText) {
+        //
+        Provider.of<Data>(context, listen: false).changeString(newText);
+      },
+    );
+  }
+}
+
+class Data extends ChangeNotifier {
+  // ChangeNotifiers exists for the purpose of NotifyingListeners
+  // for this particular class if any updates
+
+  String data = 'Top Secret Data';
+
+// always use a method to make the updates in data
+  void changeString(String newText) {
+    data = newText;
+    notifyListeners();
+    // once after updating the data we need to use notifyListeners
+    // to notify the changes who are listening to the changes
   }
 }
